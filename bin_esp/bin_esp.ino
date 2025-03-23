abd5ae85e6367c9f57eff7c1fa4bf8438d501bb9
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2025-03-23 20:08:08 +0530

    Added deep sleep code

--- bin_esp/bin_esp.ino
+++ bin_esp/bin_esp.ino
@@ -1,15 +1,20 @@
 #include "DHT.h"
 
-DHT dht(2, DHT11);
+const int dhtPin = 2;
+
+DHT dht(dhtPin, DHT11);
+
 void setup() {
-  dht.begin();
+  // dht.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Temperature in C:");  
-  Serial.println((dht.readTemperature()));  
-  Serial.println("Humidity in C:");  
-  Serial.println((dht.readHumidity()));  
-  delay(1000);  
+  // Serial.println("Temperature in C:");  
+  // Serial.println((dht.readTemperature()));  
+  // Serial.println("Humidity in C:");  
+  // Serial.println((dht.readHumidity()));  
+  delay(1000);
+  Serial.println("I'm awake, but I'm going into deep sleep mode for 10 seconds");
+  ESP.deepSleep(10e6);   
 }
