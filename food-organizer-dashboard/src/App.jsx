6647ae5108e0ba4510dc75235d1de721770c132e
santhoshparamasivam <santhoshparamasivam78@gmail.com>
2025-03-25 22:57:42 +0530

    Realtime updates added

--- food-organizer-dashboard/src/App.jsx
+++ food-organizer-dashboard/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { supabase } from './supabaseClient';
 
-function App() {
-  const [count, setCount] = useState(0)
+const Messages = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('food-bin-watch')
+      .on(
+        'postgres_changes',
+        { event: '*', schema: 'public', table: 'FoodBins' },
+        (payload) => {
+          console.log(payload),
+            setMessages((prev) => { return payload['new'] });
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ul>
+      {Object.entries(messages).map(([key, value]) => (
+        <li key={key}>
+          <strong>{key}:</strong> {String(value)}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default App
+export default Messages;
+
